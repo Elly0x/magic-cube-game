c6c4aade0d44cf1efd0728cb6dfc282b4557edce
Elly <92416739+Elly0x@users.noreply.github.com>
2026-01-08 01:06:49 -0300

    Add base files (index.html, App.tsx, index.tsx)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Magic Cube Game</h1>
+      <p>Projeto em desenvolvimento.</p>
+    </div>
+  );
+};
+
+export default App;
